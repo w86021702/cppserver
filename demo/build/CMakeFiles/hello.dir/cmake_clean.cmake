0aafb2eed1e4755808479276596f3c3fdd479b88
w86021702 <344168752@qq.com>
2016-05-20 18:55:15 +0800

    asdf

---
+++ demo/build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/democli.cpp.o"
+  "CMakeFiles/hello.dir/demo.cpp.o"
+  "CMakeFiles/hello.dir/demo1.cpp.o"
+  "hello.pdb"
+  "hello"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
