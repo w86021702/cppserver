e716658f70e653d9baa1b1e3981d352bcd319d86
w86021702 <344168752@qq.com>
2016-06-23 14:53:05 +0800

    un protobuf

--- thirdpart/protobuf/copylib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROTODIR=$HOME/appsvr/thirdparty/protobuf
-mkdir $PROTODIR/lib64_debug 
-mkdir $PROTODIR/lib64_release
-
-cp $PROTODIR/src/.libs/libprotobuf.a $PROTODIR/lib64_debug/libprotobuf.a
-cp $PROTODIR/src/.libs/libprotobuf.a $PROTODIR/lib64_release/libprotobuf.a
